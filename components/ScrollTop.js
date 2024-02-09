0818e00e6d1dcc621e5b2183d03232ce8517b596
Bijith P N <bijith.sigosoft@gmail.com>
2024-02-10 00:54:49 +0530

    utilits issue fixed

--- components/ScrollTop.js
+++ components/ScrollTop.js
@@ -1,10 +1,10 @@
 "use client";
-import { noxfolioUtilits } from "@/utility";
+import { portfolioUtilits } from "@/utility";
 import { useEffect } from "react";
 
 const ScrollTop = () => {
   useEffect(() => {
-    noxfolioUtilits.scrollTop();
+    portfolioUtilits.scrollTop();
   }, []);
 
   return (
