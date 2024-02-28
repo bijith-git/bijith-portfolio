6e29d91d1689865c30e88e1fff3403aaa49d8e9e
Bijith <bijithpn@gmail.com>
2024-02-28 19:55:03 +0530

    toast added to conatact me

--- components/Contact.js
+++ components/Contact.js
@@ -2,7 +2,7 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css";
 const Contact = () => {
   return (
     <section
@@ -175,8 +175,14 @@ export const Contact2 = () => {
       })
       .then(
         () => {
-          toast.success("Thank you for reaching out to me!", {
+          toast("Thank you for reaching out to me!🤘", {
             position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "dark",
           });
         },
         (error) => {
@@ -185,133 +191,133 @@ export const Contact2 = () => {
           });
         }
       );
-  }
-    return (
-      <section id="contact" className="contact-area py-115 rpy-100 rel z-1">
-        <div className="container">
-          <div className="row align-items-center justify-content-between">
-            <div className="col-xl-7 col-lg-8">
-              <div className="contact-two-content pt-5 rpt-0 rmb-55 wow fadeInUp delay-0-2s">
-                <div className="section-title mb-25">
-                  <span className="sub-title mb-15">
-                    <i className="flaticon-asterisk-1" /> let’s work together
-                  </span>
-                  <h2>let’s Work Together</h2>
+  };
+  return (
+    <section id="contact" className="contact-area py-115 rpy-100 rel z-1">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-xl-7 col-lg-8">
+            <div className="contact-two-content pt-5 rpt-0 rmb-55 wow fadeInUp delay-0-2s">
+              <div className="section-title mb-25">
+                <span className="sub-title mb-15">
+                  <i className="flaticon-asterisk-1" /> let’s work together
+                </span>
+                <h2>let’s Work Together</h2>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-sm-8">
+                  <h3>i,m avaiable at</h3>
+                  <h5>
+                    <i className="far fa-envelope" /> bijithpn@gmail.com
+                  </h5>
+                  <h5>
+                    <i className="far fa-phone" /> +91 7994552488
+                  </h5>
                 </div>
-                <div className="row align-items-center">
-                  <div className="col-sm-8">
-                    <h3>i,m avaiable at</h3>
-                    <h5>
-                      <i className="far fa-envelope" /> bijithpn@gmail.com
-                    </h5>
-                    <h5>
-                      <i className="far fa-phone" /> +91 7994552488
-                    </h5>
-                  </div>
-                  <div className="col-sm-4">
-                    <div className="arrow">
-                      <i className="fal fa-arrow-right" />
-                    </div>
-                  </div>
-                </div>
-                <div className="social-flow">
-                  <h4>Follow :</h4>
-                  <div className="social-style-one">
-                    <a href="https://github.com/bijith-git">
-                      <i className="fab fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/bijithpn/">
-                      <i className="fab fa-linkedin-in" />
-                    </a>
-                    <a href="https://instagram.com/b_jith__?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D">
-                      <i className="fab fa-instagram" />
-                    </a>
+                <div className="col-sm-4">
+                  <div className="arrow">
+                    <i className="fal fa-arrow-right" />
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="contact-form-two form-style-one wow fadeInUp delay-0-4s">
-                <form
-                  ref={form}
-                  onSubmit={sendEmail}
-                  id="contactForm"
-                  className="contactForm"
-                  name="contactForm"
-                  // action="assets/php/form-process.php"
-                  method="post"
-                >
-                  <div className="form-group">
-                    <label className="for-title" htmlFor="email">
-                      Email Address
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="form-control"
-                      required=""
-                      data-error="Please enter your Email"
-                    />
-                    <label htmlFor="email" className="for-icon">
-                      <i className="far fa-envelope" />
-                    </label>
-                    <div className="help-block with-errors" />
-                  </div>
-                  <div className="form-group">
-                    <label className="for-title" htmlFor="name">
-                      Your Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="form-control"
-                      required=""
-                      data-error="Please enter your Name"
-                    />
-                    <label htmlFor="name" className="for-icon">
-                      <i className="far fa-user" />
-                    </label>
-                    <div className="help-block with-errors" />
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      name="message"
-                      id="message"
-                      className="form-control"
-                      rows={7}
-                      placeholder="Write Messege....."
-                      required=""
-                      data-error="Please enter your Message"
-                      defaultValue={""}
-                    />
-                    <div className="help-block with-errors" />
-                  </div>
-                  <div className="form-group mb-0">
-                    <button type="submit" className="theme-btn">
-                      <i className="far fa-envelope" /> Messege me
-                    </button>
-                    <div id="msgSubmit" className="hidden" />
-                  </div>
-                </form>
+              <div className="social-flow">
+                <h4>Follow :</h4>
+                <div className="social-style-one">
+                  <a href="https://github.com/bijith-git">
+                    <i className="fab fa-github" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/bijithpn/">
+                    <i className="fab fa-linkedin-in" />
+                  </a>
+                  <a href="https://instagram.com/b_jith__?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D">
+                    <i className="fab fa-instagram" />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
+          <div className="col-lg-4">
+            <div className="contact-form-two form-style-one wow fadeInUp delay-0-4s">
+              <form
+                ref={form}
+                onSubmit={sendEmail}
+                id="contactForm"
+                className="contactForm"
+                name="contactForm"
+                // action="assets/php/form-process.php"
+                method="post"
+              >
+                <div className="form-group">
+                  <label className="for-title" htmlFor="email">
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                    required=""
+                    data-error="Please enter your Email"
+                  />
+                  <label htmlFor="email" className="for-icon">
+                    <i className="far fa-envelope" />
+                  </label>
+                  <div className="help-block with-errors" />
+                </div>
+                <div className="form-group">
+                  <label className="for-title" htmlFor="name">
+                    Your Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="form-control"
+                    required=""
+                    data-error="Please enter your Name"
+                  />
+                  <label htmlFor="name" className="for-icon">
+                    <i className="far fa-user" />
+                  </label>
+                  <div className="help-block with-errors" />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows={7}
+                    placeholder="Write Messege....."
+                    required=""
+                    data-error="Please enter your Message"
+                    defaultValue={""}
+                  />
+                  <div className="help-block with-errors" />
+                </div>
+                <div className="form-group mb-0">
+                  <button type="submit" className="theme-btn">
+                    <i className="far fa-envelope" /> Messege me
+                  </button>
+                  <div id="msgSubmit" className="hidden" />{" "}
+                  <ToastContainer position="top-right" />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="bg-lines">
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-          <span />
-        </div>
-      </section>
-    );
-  
-}
+      </div>
+      <div className="bg-lines">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
+    </section>
+  );
+};
